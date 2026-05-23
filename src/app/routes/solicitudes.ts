cb1014ef4e8160a4b2f7f7183f7a1088b54749bf
LauraSol77 <laura.solano@usantoto.edu.co>
2026-05-23 15:25:33 -0500

    trae las solicitudes segun rol de cada pais

--- src/app/routes/solicitudes.ts
+++ src/app/routes/solicitudes.ts
@@ -5,13 +5,11 @@ import { verifyToken } from "../../middleware/auth";
 const router = Router();
 const controller = new SolicitudController();
 
-router.get("/", (req, res) => controller.getAll(req, res));
+router.get("/", verifyToken, (req, res) => controller.getAll(req, res));
 router.get("/pendiente", verifyToken, (req, res) => controller.getPendientes(req, res));
 router.get("/gestionada", verifyToken, (req, res) => controller.getGestionadas(req, res));
 router.get("/respondida", verifyToken, (req, res) => controller.getRespondidas(req, res));
-router.get("/colombia", verifyToken, (req, res) => controller.getColombia(req, res));
-router.get("/ecuador", verifyToken, (req, res) => controller.getEcuador(req, res));
-router.get("/chile", verifyToken, (req, res) => controller.getChile(req, res));
+
 router.get("/:id", (req, res) => controller.getById(req, res));
 router.post("/", verifyToken, (req, res) => controller.create(req, res));
 router.put("/:id", verifyToken, (req, res) => controller.update(req, res));
