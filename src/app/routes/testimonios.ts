372d6d2826aa1a0979dc07ca5ada577b3a1292af
WldySandoval1 <wldysandovalgarzon@gmail.com>
2026-05-23 22:10:20 -0500

    endpoint de total testimonios por pais y recibiendo parametro del front para filtrar

--- src/app/routes/testimonios.ts
+++ src/app/routes/testimonios.ts
@@ -10,6 +10,11 @@ router.get("/:id", (req, res) => controller.getById(req, res));
 router.post("/", verifyToken, (req, res) => controller.create(req, res));
 router.put("/:id", verifyToken, (req, res) => controller.update(req, res));
 router.delete("/:id", verifyToken, (req, res) => controller.delete(req, res));
-router.get("/stats/total-publicados", (req, res) => controller.getTotalPublicados(req, res));
+router.get("/stats/total-publicados", (req, res) =>
+  controller.getTotalPublicados(req, res),
+);
+router.get("/stats/total-publicados-pais", verifyToken, (req, res) =>
+  controller.getTotalTestimoniosPais(req, res),
+);
 
 export default router;
